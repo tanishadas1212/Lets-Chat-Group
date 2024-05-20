3783e748109433b5ea279736856008395cd5e11c
tanishadas1212 <110232710+tanishadas1212@users.noreply.github.com>
2024-05-20 16:08:16 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCFADb8meCmFOXYcMCDku5t5OvaAEYMfaA",
+    authDomain: "kwitter-2d53b.firebaseapp.com",
+    databaseURL: "https://kwitter-2d53b-default-rtdb.firebaseio.com",
+    projectId: "kwitter-2d53b",
+    storageBucket: "kwitter-2d53b.appspot.com",
+    messagingSenderId: "636546913037",
+    appId: "1:636546913037:web:a264b2b16c572934dbd644"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
